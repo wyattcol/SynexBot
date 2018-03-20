196ab4b9297a2acf48024f8180efe9274b9c104d
wyattcol <35353860+wyattcol@users.noreply.github.com>
2018-03-20 16:26:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,20 +9,30 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+// client.on('message', message => {
+// 	var words = message.content.split(' ')
+// 	if (words[0] === ':announce') {
+// 		var ClientsWords = words
+// 		id = message.author.id
+// 		tag = message.author.tag
+// 		console.log(message.author.id)
+// 		console.log(message.author.username)
+// 		username = message.author.username
+// 		replacedmessage = message + message: txt.replace(":announce","")
+// // 		message.guild.channels.find("name", "🚔announcements🚔").send(replacedmessage);
+// 		sendMessage({ to: 🚔announcements🚔, message: txt.replace(':announce ','') 
+// // 		client.message.send({ to: 🚔announcements🚔, message: text.replace(':announce ','') 
+
+// 	}
+// });
+
 client.on('message', message => {
 	var words = message.content.split(' ')
-	if (words[0] === ':announce') {
-		var ClientsWords = words
-		id = message.author.id
-		tag = message.author.tag
-		console.log(message.author.id)
-		console.log(message.author.username)
-		username = message.author.username
-		replacedmessage = message + message: txt.replace(":announce","")
-// 		message.guild.channels.find("name", "🚔announcements🚔").send(replacedmessage);
-		sendMessage({ to: 🚔announcements🚔, message: txt.replace(':announce ','') 
-// 		client.message.send({ to: 🚔announcements🚔, message: text.replace(':announce ','') 
-
+	if (words[0] === ':announce')
+	{
+	let botmessage = args.join(" ");
+	message.delete().catch();
+	message.channel.send(botmessage)
 	}
 });
 
