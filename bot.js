835e79370d6c93ea63382ccde10a806f983c7280
wyattcol <35353860+wyattcol@users.noreply.github.com>
2018-03-25 18:37:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("message", (message) => {
-    let args = message.content.split(" ").slice(1);    
-    if (message.content.startsWith(";announce")){    
-     client.channels.get("425397615778201630").send({embed: {
-            color: 3066993,
-            author: {
-              name: client.user.username,
-              icon_url: client.user.avatarURL
-            },
-            title: "Announcement",
-            url: "",
-            description: "This is an important announcement",
-            fields: [{
-                name: "Announcement",
-                value: args
-              }
-            ],
-            timestamp: new Date(),
-            footer: {
-              icon_url: client.user.avatarURL,
-              text: " "
-            }
-          }
-        });
-    }
-});
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(sayMessage);
+
+}
+
+module.exports.help = {
+  name: "say"
+
 
 client.login('pNDI1Mzk0OTMyNTgxNDAwNTc2.DZRJxA.8JK3KG_bO9WXjoFQG-3icFyIjGo');
