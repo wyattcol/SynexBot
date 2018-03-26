ee305be6e37e63ce044a55ac27ba9b4bb22ba092
wyattcol <35353860+wyattcol@users.noreply.github.com>
2018-03-26 16:28:10 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,54 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
-module.exports.run = async (bot, message, args) => {
-
-      if(!message.member.hasPermission("ADMINISTRATOR")) return;
-      const sayMessage = args.join(" ");
-      message.delete().catch();
-      message.channel.send(sayMessage);
-
-}
-
-module.exports.help = {
-  name: "say"
+const token = 'pNDI1Mzk0OTMyNTgxNDAwNTc2.DZRJxA.8JK3KG_bO9WXjoFQG-3icFyIjGo';
 
 
-client.login('pNDI1Mzk0OTMyNTgxNDAwNTc2.DZRJxA.8JK3KG_bO9WXjoFQG-3icFyIjGo');
+
+
+
+ScriptLink = "http://fasttory.com/CnbU"
+GameLink = "https://www.roblox.com/games/13822889/Lumber-Tycoon-2"
+VideoLink = ""
+
+
+
+
+
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+	var words = message.content.split(' ')
+	if (words[0] === ':runmessage') {
+message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "A new exploit from End Gaming!",
+    url: VideoLink,
+    description: "Here are some instructions to go along with the exploit",
+    fields: [{
+        name: "Link to script",
+        value: ScriptLink
+      },
+      {
+        name: "Game Link",
+        value: GameLink
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: " "
+    }
+  }
+});
+
+	}
+});
+
+client.login(token);
