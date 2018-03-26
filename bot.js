69d5e8c2905854de9ef901d7654ef997052b4181
wyattcol <35353860+wyattcol@users.noreply.github.com>
2018-03-26 16:31:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = 'NDI1Mzk0OTMyNTgxNDAwNTc2.DZrplQ.iGfza2lZiT6tse-t6zcaMFSdBHo';
-
-
-
-
-
-ScriptLink = "http://fasttory.com/CnbU"
-GameLink = "https://www.roblox.com/games/13822889/Lumber-Tycoon-2"
 VideoLink = ""
-
-
-
-
 
 
 client.on('ready', () => {
@@ -20,24 +9,20 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	var words = message.content.split(' ')
-	if (words[0] === ':runmessage') {
+	let args = message.content.split(" ").slice(0);
+	if (words[0] === ':announce') {
 message.channel.send({embed: {
     color: 3447003,
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "A new exploit from End Gaming!",
+    title: "Announcement",
     url: VideoLink,
-    description: "Here are some instructions to go along with the exploit",
+    description: "This is an important Announcement",
     fields: [{
-        name: "Link to script",
-        value: ScriptLink
-      },
-      {
-        name: "Game Link",
-        value: GameLink
+        name: "Announcement",
+        value: args
       }
     ],
     timestamp: new Date(),
